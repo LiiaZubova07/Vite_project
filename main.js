e1bf858fd68c14682a902e5a72d34d3670a81085
Liia Zubova <vardlitmo@gmail.com>
2023-01-27 17:25:35 +0300

    add

--- main.js
+++ main.js
@@ -59,11 +59,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   /*-----------------------------------закрытие на ESC-------------------------*/
   document.addEventListener(
-    'keyup',
+    'keydown',
     (e) => {
-      const key = e.keyCode;
-
-      if (key == 27) {
+      if (e.key == 'Escape') {
         document.querySelector('.modal.active').classList.remove('active');
         document.querySelector('.overlay').classList.remove('active');
       }
